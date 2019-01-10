a5234f6170dcfde297c9b8748bf40bcbc6439309
TacB0sS <adam.vdk@nu-art-software.com>
2019-01-10 16:51:33 +0200

    bai script

--- build-and-install.sh
+++ build-and-install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 bash dev-tools/scripts/dev/build-and-install.sh \
+                --app-name=your-apk-name \
                 --package-name=com.your.app \
                 --build=debug \
                 --project=app \
